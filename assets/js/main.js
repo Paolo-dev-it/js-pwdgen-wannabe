2326a01976511491bb2b04fdc2622b897f6e096b
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-18 14:33:44 +0200

    Created password with variables. Project completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,20 @@
 
 
 
-let nome = 'Paolo';
-console.log(nome);
+
+let name = prompt('Write your name');
+console.log(name);
+
+let surname = prompt('Write your surname');
+console.log(surname);
+
+let colors = prompt('Write your favourite color');
+console.log(colors);
+
+const numbers = 99;
+console.log(numbers);
+
+document.getElementById('password').innerHTML = `la tua password insicurissima è ${name + surname + colors + numbers}`
 
 
 
