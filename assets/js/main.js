48bc6ac3dd812a02d43bc9331cd4f0b7e35e0703
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-10-18 15:03:05 +0200

    Added comments. Project completed.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 
 
-let name = prompt('Write your name');
+let name = prompt('Write your name'); //Variabile Nome
 console.log(name);
 
-let surname = prompt('Write your surname');
+let surname = prompt('Write your surname'); //Variabile Cogome
 console.log(surname);
 
-let colors = prompt('Write your favourite color');
+let colors = prompt('Write your favourite color'); //Variabile Colore
 console.log(colors);
 
-const numbers = 99;
+const numbers = 99; //Variabile Numero
 console.log(numbers);
 
-document.getElementById('password').innerHTML = `La tua password insicurissima è ${name + surname + colors + numbers}`
+document.getElementById('password').innerHTML = `La tua password insicurissima è ${name + surname + colors + numbers}` // Concatenazione testuale
 
 
 
